fc327eab6d5b3a402b4ade7ba0a04c658bbcd535
paulapgp <paula@hotmail.com>
2017-07-11 15:53:32 +0200

    cambio nombre interfaz

---
+++ Ejer08/CalculadoraConFichero.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejer08
+{
+    public class CalculadoraConFichero
+    {
+
+        public ICalculadora calculadora
+        {
+            get;
+            set;
+        }
+
+        public IFicheroService fichero
+        {
+            get;
+            set;
+        }
+
+        public void SumarGuardando(int a, int b)
+        {
+            int c = calculadora.Sumar(a, b);
+
+            fichero.GuardarNumero(c);
+        }        
+    }
+}
---
+++ Ejercicio08Test/CalculadoraConFicheroTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ejer08;
+
+namespace Ejercicio08Test
+{
+    [TestClass]
+    public class CalculadoraConFicheroTest
+    {
+        [TestMethod]
+        public void TestSumarGuardando()
+        {
+            CalculadoraConFichero sut = new CalculadoraConFichero();
+            sut.calculadora = new CalculadoraDel9();
+            sut.fichero = new FicheroServiceDeMentira();
+            sut.SumarGuardando(4, 5);
+        }
+    }
+
+    public class CalculadoraDel9 : ICalculadora
+    {
+            public int Sumar(int a, int b)
+            {
+                return 9;
+            }
+   }
+
+    public class FicheroServiceDeMentira : IFicheroService
+    {
+        public void GuardarNumero(int c)
+        {
+            
+        }
+    }
+
+
+}
